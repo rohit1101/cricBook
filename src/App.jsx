c2408ad5476c506b8eb6806e202e0ac6d11b6557
rohit1101 <srohit1101@gmail.com>
2020-08-24 11:32:59 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import "./App.css"
+import helper from "./helpers/querying.js"
 
 function App() {
   return <div className="App">Playing with queries</div>
---
+++ src/helpers/querying.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Query() {
+  return (
+    <div>
+      <input type="text" />
+      <button>Add Post</button>
+    </div>
+  )
+}
+
+export default Query
