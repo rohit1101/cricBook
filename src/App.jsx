9f035a7ff9cb3ade428a96b20c383afcaaf2b559
rohit1101 <srohit1101@gmail.com>
2020-09-03 21:58:55 +0530

    auth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
 import React from "react"
+import firebase from "firebase/app"
+import "firebase/auth"
+// import "firebase/firestore"
+
+import firebaseConfig from "./firebaseConfig"
+firebase.initializeApp(firebaseConfig)
 
 class App extends React.Component {
+  state = {
+    loginState: false,
+    user_arr: [],
+  }
+
+  handleAuthClick = (e) => {
+    let provider = new firebase.auth.GoogleAuthProvider()
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        let token = result.credential.accessToken
+        let user = result.user
+        console.log(user.email, user.uid, user.displayName)
+      })
+
+    // this.setState({ user_arr: user })
+    // this.setState({ loginState: !this.state.loginState })
+  }
+
   render() {
-    return <h1>Auth page</h1>
+    return (
+      <div>
+        <button onClick={this.handleAuthClick}>Login with Google</button>
+        {this.state.loginState && <h1>Welcome to home page</h1>}
+      </div>
+    )
   }
 }
 
