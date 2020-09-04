1e437b43529c3a39cfb0f5e0d3e00a9a3fd3c186
rohit1101 <srohit1101@gmail.com>
2020-09-04 21:54:59 +0530

    firebase auth persistance commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,23 @@ firebase.initializeApp(firebaseConfig)
 class App extends React.Component {
   state = {
     loginState: false,
-    user_arr: [],
+    user_arr: JSON.parse(localStorage.getItem("user_arr")) || [],
   }
 
   handleAuthClick = async (e) => {
-    googleAuthHandler()
-
     const userData = await googleAuthHandler()
-    console.log(userData)
+
     const userInfo = Object.values(userData)
     console.log(userInfo[2])
-
     this.setState({ user_arr: [userInfo[2].profile] })
+    localStorage.setItem("user_arr", JSON.stringify([userInfo[2].profile]))
     this.setState({ loginState: true })
   }
 
   render() {
     return (
       <div>
-        {this.state.loginState ? (
+        {this.state.user_arr.length ? (
           <h1>Welcome to home page</h1>
         ) : (
           <button onClick={this.handleAuthClick}>Login with Google</button>
--- src/AuthConfig.js
+++ src/AuthConfig.js
@@ -4,14 +4,7 @@ import "firebase/auth"
 export async function googleAuthHandler() {
   let provider = new firebase.auth.GoogleAuthProvider()
 
-  let data = await firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(function (result) {
-      let token = result.credential.accessToken
-      let user = result.user
-      console.log(user.email, user.uid, user.displayName)
-    })
+  let data = await firebase.auth().signInWithPopup(provider)
 
   return data
 }
