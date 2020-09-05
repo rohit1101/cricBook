fda405405b91c4959f748b8a36a682a365ad479d
rohit1101 <srohit1101@gmail.com>
2020-09-05 11:53:41 +0530

    firebase auth persistance commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,35 @@ firebase.initializeApp(firebaseConfig)
 
 class App extends React.Component {
   state = {
-    loginState: false,
     user_arr: JSON.parse(localStorage.getItem("user_arr")) || [],
   }
 
   handleAuthClick = async (e) => {
     const userData = await googleAuthHandler()
-
     const userInfo = Object.values(userData)
-    console.log(userInfo[2])
     this.setState({ user_arr: [userInfo[2].profile] })
-    localStorage.setItem("user_arr", JSON.stringify([userInfo[2].profile]))
-    this.setState({ loginState: true })
+    localStorage.setItem("user_arr", JSON.stringify(this.state.user_arr))
+  }
+
+  handleLogOutClick = (e) => {
+    this.setState({ user_arr: [] })
+  }
+
+  componentDidUpdate = (prevState) => {
+    if (prevState.user_arr !== this.state.user_arr) {
+      localStorage.setItem("user_arr", JSON.stringify(this.state.user_arr))
+    }
+    console.log("state changed")
   }
 
   render() {
     return (
       <div>
         {this.state.user_arr.length ? (
-          <h1>Welcome to home page</h1>
+          <div>
+            <p>Welcome to Home page.</p>
+            <button onClick={this.handleLogOutClick}>Log Out</button>
+          </div>
         ) : (
           <button onClick={this.handleAuthClick}>Login with Google</button>
         )}
