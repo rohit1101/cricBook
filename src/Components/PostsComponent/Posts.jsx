ea52e1278441a0061c09bc672f5b757248a3e2ec
rohit1101 <srohit1101@gmail.com>
2020-09-10 17:56:48 +0530

    async functional component bug

--- src/Components/PostsComponent/Posts.jsx
+++ src/Components/PostsComponent/Posts.jsx
@@ -1,20 +1,16 @@
 import React from "react"
 import getAllPosts from "../../helpers/allPosts"
 
-// class Posts extends React.Component {
-//   async componentDidMount() {
-//     const d = await getAllPosts()
-//     console.log(d)
-//   }
-//   render() {
-//     return <p>All posts</p>
-//   }
-// }
-
-async function Posts() {
-  const d = await getAllPosts()
-  console.log(d)
-  return <p>ALl posts</p>
+class Posts extends React.Component {
+  async componentDidMount() {
+    const d = await getAllPosts()
+    console.log(d)
+  }
+  render() {
+    return <p>All posts</p>
+  }
 }
+
+
 
 export default Posts
