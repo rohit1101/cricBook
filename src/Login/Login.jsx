c123e95ae3deda2bd3bfd1b9a26aad1d60375848
rohit1101 <srohit1101@gmail.com>
2020-10-09 16:41:08 +0530

    login page initial commit

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -2,9 +2,11 @@ import React from "react"
 import { navigate, Redirect } from "@reach/router"
 import { googleAuthHandler } from "../firebaseConfig"
 import userLogin from "../helpers/userLogin"
-
+import styles from "./Login.module.scss"
+import image from "../authentication.svg"
+import logo from "../logo.png"
 class Login extends React.Component {
-  handleAuthClick = async (e) => {
+  handleAuthClick = async () => {
     const userData = await googleAuthHandler()
     const userInfo = Object.values(userData)
     userLogin(userInfo[2].profile)
@@ -18,15 +20,26 @@ class Login extends React.Component {
         {localStorage.user_arr ? (
           <Redirect to="/home" noThrow />
         ) : (
-          <div>
-            <h1>CricBook</h1>
-            <p>Your Opinion of Cricket!</p>
-            <button
-              onClick={this.handleAuthClick}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Login with Google
-            </button>
+          <div className={styles.header}>
+            <div className={styles.logo}>
+              <img src={logo} alt="logo" />
+            </div>
+            <div className={styles.content}>
+              <div className={styles.text}>
+                <h1>CricBook</h1>
+                <p>Social Media for Cricket Freaks</p>
+                <button
+                  onClick={this.handleAuthClick}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Login with Google
+                </button>
+              </div>
+
+              <div className={styles.image}>
+                <img src={image} alt="login" />
+              </div>
+            </div>
           </div>
         )}
       </div>
